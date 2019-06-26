6f9ce3db75ff20568b047b334f843ddd056725ee
Judy W <judy.wm.wong@gmail.com>
2019-06-26 10:37:00 -0400

    Add index css file and global animation styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,17 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "./index.css"
 
 const IndexPage = ( {data} ) => (
   <Layout>
     <SEO title="Home" />
 
-    <h1>well, hello!</h1>
+    <h1 style = {{
+      paddingTop: '50px',
+    }}> 
+      well, hello!
+    </h1>
     <p>i'm judy wong. nyc-based designer & developer.</p>
 
     <p>check out some of my work <a href="/projects">here</a> and learn 
