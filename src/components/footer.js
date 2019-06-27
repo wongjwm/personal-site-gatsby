65a3c5aff12bb3b27d37b82114b2ba406ea6930b
Judy W <judy.wm.wong@gmail.com>
2019-06-27 16:34:30 -0400

    Add social media links to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,10 +5,19 @@ import "./footer.css"
 
 const Footer = () => (
   <footer>
-    © {new Date().getFullYear()}, designed & developed by judy wong. 
-    built with
-    {` `}
-    <a href="https://www.gatsbyjs.org">gatsby</a>
+
+    <div className="footer-left-content">
+      © {new Date().getFullYear()}, designed & developed by judy wong. 
+      built with
+      {` `}
+      <a href="https://www.gatsbyjs.org">gatsby</a>
+    </div>
+
+    <div className="social-media-links">
+      <a href="mailto:wong.ju@husky.neu.edu">email</a>
+      <a href="https://www.linkedin.com/in/judy-wm-wong/">linkedin</a>
+      <a href="https://github.com/wongjwm">github</a>
+    </div>
   </footer>
 )
 
